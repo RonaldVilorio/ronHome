82315102c75a3bc04f4c2b90ae102fce28c9a347
RonaldVilorio <ronvilorio@gmail.com>
2022-07-04 10:01:09 -0400

    cleaned up index page, added about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import {Link} from 'gatsby'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <Link to='/'>Home</Link>
+        </main>
+    )
+}
+export default AboutPage
