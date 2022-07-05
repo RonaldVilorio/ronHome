936ad324a694c282fd769ebd1351f73048dcb979
RonaldVilorio <ronvilorio@gmail.com>
2022-07-05 14:45:47 -0400

    Theme component added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import * as React from "react"
 import Layout from '../components/layout/layout.component'
 import Header from "../components/header/header.component"
+import Theme from "../components/Theme.component"
+import styled from 'styled-components';
+
 const IndexPage = () => {
   return (
-    <Layout pageTitle = 'Home Page'>
-      <Header/>
-      {/* Projects section */}
-      {/* Footer section */}
+    <Theme>
+      <Layout pageTitle = 'Home Page'>
+        <Header/>
+        {/* Projects section */}
+        {/* Footer section */}
 
-     
-    </Layout>
+      </Layout>
+    </Theme>
     
   )
 }
