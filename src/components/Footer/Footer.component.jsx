e2b147168f3d935f46497482a12b99addc9480f4
RonaldVilorio <ronvilorio@gmail.com>
2022-07-14 14:35:33 -0400

    footer elements aligned

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { FooterBox } from "./Footer.styles";
+import { FooterBox,EndingInfo } from "./Footer.styles";
 
 const Footer = () => {
     return ( 
         <>
             <FooterBox>
-                <h1>
-                    <span>Contact Me: ronvilorio@gmail.com</span>
-                    <span><a href="https://github.com/RonaldVilorio/ronHome">Site Source Code</a></span>
-                </h1>
+                <EndingInfo>
+                    <h1>
+                        <span>Contact Me: ronvilorio@gmail.com</span>
+                        <span><a href="https://github.com/RonaldVilorio/ronHome">Site Source Code</a></span>
+                    </h1>
+                </EndingInfo>
                 <p>&#169; 2022 Ronald Vilorio</p>
             </FooterBox>
         {/* Contact me */}
--- src/components/Footer/Footer.styles.jsx
+++ src/components/Footer/Footer.styles.jsx
@@ -3,13 +3,32 @@ import { Colors } from "../GlobalStyle";
 
 export const FooterBox = styled.div`
     display:flex;
-    justify-content:center;
+    flex-direction:column;
     background-color: ${Colors.black_bean};
     padding:.2rem;
-    height: 20rem;
+    height: auto;
     color:${Colors.primary_green};
     text-transform:uppercase;
-    line-height:3rem;
+    
+    
+    p{
+        /* display:inline-block; */
+        margin-top:0;
+        margin-left:1.5rem;
+        align-self:flex-start;
+        font-size:1.6rem;
+
+    }
+    
+`
+export const EndingInfo = styled.div`
+
+    padding:2rem;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    transform:translateY(1rem);
+
     h1{
         display:flex; 
         justify-content:center;
@@ -17,19 +36,12 @@ export const FooterBox = styled.div`
         display:flex;
         flex-direction:column;
         font-size:2.2rem;
-        transform:translateX(10rem);
+        line-height:3rem;
+        
         a{
             text-decoration:none;
             color:beige;
         }
     }
-    p{
-        color:blue;
-        
-        
-
-    }
-    
-    
 
 `
